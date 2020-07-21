d05fcff1cb68d3985d06590ff968086ed5bdf370
david-diep <ddkotan@gmail.com>
2020-07-20 18:05:38 -0700

    user can naviagate different views

---
+++ src/Components/view-cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ViewCards extends React.Component {
+  render() {
+    return (<>
+      <h1 className="text-center">View Cards</h1>
+    </>);
+  }
+}
+
+export default ViewCards;
